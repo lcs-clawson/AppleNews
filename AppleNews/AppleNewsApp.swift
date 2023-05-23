07f0b5b7b65e0f9963b0c176aa0aebbe876a49a2
Cooper Lawson <CSLawson@lcs.on.ca>
2023-05-23 11:35:18 -0400

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by Cooper Lawson on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
